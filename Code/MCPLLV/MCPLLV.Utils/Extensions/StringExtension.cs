25b2c7d7b4757e40f95b916ee283aa1d5c83941d
suonlight <minh.nh1989@gmail.com>
2013-03-13 12:55:41 +0000

    initial code structure

---
+++ Code/MCPLLV/MCPLLV.Utils/Extensions/StringExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCPLLV.Utils.Extensions
+{
+    public static class StringExtension
+    {
+        public static string ToDBConventionName(this string input)
+        {
+            // TODO: normalize input to table name
+            return input;
+        }
+
+        public static string ToRemovedUnicode(this string input)
+        { 
+            // TODO: remove and replace Unicode characters
+            return input;
+        }
+    }
+}
